7994bbac36114ef094c6812b7c46dd0a8d2f58fb
gjasay <gabeasay@gmail.com>
2024-11-01 19:39:09 -0400

    header bug-fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,6 +24,8 @@ const Header: React.FC = () =>
         className="fixed top-0 right-0 h-full w-full text-5xl bg-tvGreen shadow-lg flex flex-col justify-around py-4 md:hidden z-20"
       >
         <div></div>
+        <div></div>
+        <div></div>
         <li className="text-center px-4 py-8">
           <a
             onClick={() => setOpen(!isOpen)}
