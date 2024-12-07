8f934954d3415ef063c553a183717c170d609ea9
gjasay <gabeasay@gmail.com>
2024-12-07 17:41:42 -0500

    updated main menu and added tiny bites menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,14 +2,22 @@ const Menu = () => {
   return (
     <div
       id="menu"
-      className="flex flex-col justify-center items-center py-8 h-screen"
+      className="flex flex-col justify-center items-center py-8 h-full"
     >
       <h1 className="text-center font-bold text-4xl my-8">Menu</h1>
-      <img
-        src="./menu.jpg"
-        alt="menu"
-        className="object-fit w-11/12 md:w-1/3  rounded-lg border-black border-4"
-      />
+      <div className="flex flex-col md:flex-row justify-center">
+        <img
+          src="./menu.jpeg"
+          alt="menu"
+          className="object-fit w-11/12 md:w-1/3  rounded-lg border-black border-4 m-4"
+        />
+        <img
+          src="./tiny_menu.jpeg"
+          alt="menu"
+          className="object-fit w-11/12 md:w-1/3  rounded-lg border-black border-4 m-4"
+        />
+      </div>
+
     </div>
   );
 };
