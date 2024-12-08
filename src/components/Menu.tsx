dc3e6534183d7719e9577fb0159a51c4c7555c53
gjasay <gabeasay@gmail.com>
2024-12-08 18:49:30 -0500

    menu fix

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,22 +2,21 @@ const Menu = () => {
   return (
     <div
       id="menu"
-      className="flex flex-col justify-center items-center py-8 h-full"
+      className="flex flex-col justify-center items-center py-8 h-screen"
     >
       <h1 className="text-center font-bold text-4xl my-8">Menu</h1>
-      <div className="flex flex-col md:flex-row justify-center">
+      <div className="flex flex-col md:flex-row justify-center items-center h-full">
         <img
           src="./menu.jpeg"
           alt="menu"
-          className="object-fit w-11/12 md:w-1/3  rounded-lg border-black border-4 m-4"
+          className="object-fit w-3/5 md:w-1/3  rounded-lg border-black border-4 m-4"
         />
         <img
           src="./tiny_menu.jpeg"
           alt="menu"
-          className="object-fit w-11/12 md:w-1/3  rounded-lg border-black border-4 m-4"
+          className="object-fit w-3/5 md:w-1/3  rounded-lg border-black border-4 m-4"
         />
       </div>
-
     </div>
   );
 };
