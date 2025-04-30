e034375ffd72aecbc901b36738ecb3b539a60583
gjasay <gabeasay@gmail.com>
2025-04-30 12:00:41 -0400

    update hours & menu

--- src/components/Hours.tsx
+++ src/components/Hours.tsx
@@ -7,13 +7,13 @@ const Hours = () =>
     <div id="hours" className="py-6 h-screen">
       <h1 className="text-4xl font-bold text-center pb-6">Hours</h1>
       <div className="flex flex-col justify-center items-center">
-        <p className="text-xl">Monday: Closed</p>
+        <p className="text-xl">Monday: 8am - 12pm</p>
         <p className="text-xl">Tuesday: Closed</p>
-        <p className="text-xl">Wednesday: 12pm - 3pm</p>
-        <p className="text-xl">Thursday: 9am - 3pm</p>
-        <p className="text-xl">Friday: 9am - 3pm</p>
-        <p className="text-xl">Saturday: 9am - 3pm</p>
-        <p className="text-xl">Sunday: 9am - 3pm</p>
+        <p className="text-xl">Wednesday: 10am - 3pm</p>
+        <p className="text-xl">Thursday: 10am - 3pm</p>
+        <p className="text-xl">Friday: 10am - 3pm</p>
+        <p className="text-xl">Saturday: 9am - 4pm</p>
+        <p className="text-xl">Sunday: 10am - 3pm</p>
       </div>
       <h1 className="text-4xl font-bold text-center py-6">Address</h1>
       <div className="flex flex-col justify-center items-center">
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,7 +7,7 @@ const Menu = () => {
       <h1 className="text-center font-bold text-4xl my-4">Menu</h1>
       <div className="flex flex-col md:flex-row justify-center items-center h-full">
         <img
-          src="./menu.jpeg"
+          src="./menu.png"
           alt="menu"
           className="object-fit rounded-lg border-black border-4 m-4"
         />
